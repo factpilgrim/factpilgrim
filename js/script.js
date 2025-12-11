0c662d5c203bd841ad5fb00b652182f86c7e6381
FACT PILGRIM <245856648+factpilgrim@users.noreply.github.com>
2025-12-11 07:39:12 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -218,7 +218,7 @@ window.open(`./articles/${filename}`, '_blank');
 });
 
 const width = span1.offsetWidth;
-const duration = width / 100; // Increased from 50 to 100 for faster speed
+const duration = width / 150; // Increased from 100 to 150 for even faster speed
 tickerTrack.style.setProperty('--ticker-duration', `${duration}s`);
 }
 
